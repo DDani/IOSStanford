e90dcfb8c2cc656a2f9baeb5d70ac0afbd5e9c38
DDani <danicastro@gmail.com>
2014-01-23 16:26:47 +0100

    Initial commit from local git

---
+++ Matchismo/Matchismo/CardGameViewController.h
@@ -0,0 +1,13 @@
+//
+//  CardGameViewController.h
+//  Matchismo
+//
+//  Created by Dani Castro on 23/01/14.
+//  Copyright (c) 2014 Dani Castro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CardGameViewController : UIViewController
+
+@end
---
+++ Matchismo/Matchismo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Matchismo
+//
+//  Created by Dani Castro on 23/01/14.
+//  Copyright (c) 2014 Dani Castro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CardGameAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CardGameAppDelegate class]));
+    }
+}
